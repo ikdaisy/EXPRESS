abd995e3f80a7efb100aeed276ca80cd63e53023
safa <safakallianthodi6@gmail.com>
2024-09-10 13:54:14 +0530

    Added

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require("express")
 const app = express()
 const PORT=3002
-const bodyParser=require ("body-parser")
+const path= require("path")
 
+// const bodyParser=require ("body-parser")
 // app.use(bodyParser.urlencoded())
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
+
+//to load all pages from the same folder 
+// app.use(express.static("pages"))
+// app.use(express.urlencoded());
 
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
-
-app.post("/about",(req,res)=>{
-    console.log(req.body);
-    res.status(201).send("About page")
-  
+app.get("/",Token,(req,res)=>{
+    console.log("Router");
+    res.send("Hello")
+    // console.log(__dirname);
+    // res.sendFile(path.join(__dirname,"pages","index.html"))  
     
 })
+
+function Token(req,res,next){
+    console.log("Middle ware");
+    next()
+    
+}
+
+// app.post("/about",(req,res)=>{
+//     console.log(req.body);
+//     res.status(201).send("About page")
+// })
 
 app.get("*",(req,res)=>{
     res.status(404).send("Invalid")
